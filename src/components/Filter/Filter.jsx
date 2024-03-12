e565bf9489d7fffd2df8d823265f4a2dfe3bb0dd
Sameer <sameersattar1111@gmail.com>
2024-03-12 07:25:54 +0500

    Filter component complete

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import "./Filter.scss"
+import React from "react";
+import "./Filter.scss";
+import search from "../../assets/search.png"
 
 const Filter = () => {
   return (
-    <div>Filter</div>
-  )
-}
+    <div className="filter">
+      <h1>
+        Search results for <b>Location</b>
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="City location"
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="buy">Buy</option>
+            <option value="">any</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="apartment">Apartment</option>
+            <option value="">any</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            name="minPrice"
+            id="minPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            name="maxPrice"
+            id="maxPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input type="number" name="bedroom" id="bedroom" placeholder="any" />
+        </div>
+        <button>
+          <img src={search} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Filter
+export default Filter;
