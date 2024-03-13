73bbba0a678ad58f8b364ac4572f4cb006df5d89
Sameer <sameersattar1111@gmail.com>
2024-03-13 22:38:51 +0500

    card componenet complete

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-import "./Card.scss"
+import React from "react";
+import "./Card.scss";
+import bed from "../../assets/bed.png";
+import bath from "../../assets/bath.png";
+import pin from "../../assets/pin.png";
+import chat from "../../assets/chat.png";
+import save from "../../assets/save.png";
+import { Link } from "react-router-dom";
 
-
-const Card = () => {
+const Card = ({ item }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>
+            {item.title}
+          </Link>
+        </h2>
+        <p className="address">
+          <img src={pin} alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src={bed} alt="" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src={bath} alt="" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
 
-export default Card
+          <div className="icons">
+            <div className="icon">
+              <img src={save} alt="" />
+            </div>
+            <div className="icon">
+              <img src={chat} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
