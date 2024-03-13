87ef7fc8fc60c2fa4cc1b28432f600d2532c8f25
Sameer <sameersattar1111@gmail.com>
2024-03-13 22:39:16 +0500

    complete

--- src/routes/ListPage/ListPage.jsx
+++ src/routes/ListPage/ListPage.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Map from '../../components/Map/Map'
+import {listData} from "../../data/data"
+import Filter from '../../components/Filter/Filter'
+import Card from '../../components/Card/Card'
+import "./ListPage.scss"
+
 
 const ListPage = () => {
+  const data = listData;
   return (
-    <div>ListPage</div>
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter/>
+          {
+            data.map((item) => {
+              return (
+                <Card  item ={item} key={item.id} />
+              )
+            })
+          }
+        </div>
+      </div>
+      <div className="mapContainer">
+        <Map items={data}/>
+      </div>
+    </div>
   )
 }
 
