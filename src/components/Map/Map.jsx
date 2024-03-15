4d8097b18d67e517d0949803a8470bc27a6760c7
Sameer <sameersattar1111@gmail.com>
2024-03-15 16:09:24 +0500

    map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import "./Map.scss"
+import "leaflet/dist/leaflet.css";
+import Pin from '../Pin/Pin';
 
-const Map = ({items}) => {
+const Map = ({items}) => { 
+
   return (
-    <div>Map</div>
+    <div>
+      <MapContainer className='map' center={[51.505, -0.09]} zoom={7} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {
+      items.map((item) => {
+        return (
+          <Pin item={item} key={item.id}/>
+        )
+      })
+    }
+  </MapContainer>
+
+    </div>
   )
 }
 
