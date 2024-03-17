b0ca7cabda4ea1cd92fca36065a34da4c4e434c7
Sameer <sameersattar1111@gmail.com>
2024-03-17 12:22:55 +0500

    slider component complete

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
-import "./Slider.scss"
+import React, { useState } from "react";
+import "./Slider.scss";
+import arrow from "../../assets/arrow.png";
 
-const Slider = ({images}) => {
+const Slider = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(null);
+
+  const changeSlide = (direction) => {
+    if (direction === "next") {
+      if (imageIndex === images.length - 1) {
+        setImageIndex(0)
+      } else {
+        setImageIndex(imageIndex+1)
+      }
+    } else {
+        if (imageIndex === 0) {
+          setImageIndex(images.length -1)
+        } else {
+          setImageIndex(imageIndex - 1)
+        }
+    }
+    
+  }
+
   return (
-    <div className='slider'>
+    <div className="slider">
+      {imageIndex !== null && open &&  (
+        <div className="fullSlider">
+          <div className="arrow" onClick={() => changeSlide("back")}>
+            <img src={arrow} alt="" />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow" onClick={() => changeSlide("next")}>
+            <img src={arrow} className="right" alt="" />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>X</div>
+        </div>
+      )}
+
       <div className="bigImage">
-        <img src={images[0]} alt="" />
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
       <div className="smallImages">
-        {
-          images.slice(1).map((image , index) => {
-            return(
-              <img
-              src={image}
-              alt='images'
-              key={index}
-              />
-            )
-          })
-        }
+        {images.slice(1).map((image, index) => {
+          return <img src={image} alt="images" key={index}  onClick={() => setImageIndex(index+1)} />;
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
