17c1aef7ba3ae39927407a5f7b2eaebaf03a4902
Sameer <sameersattar1111@gmail.com>
2024-03-28 20:50:17 +0500

    profile component

--- src/routes/ProfilePage/ProfilePage.jsx
+++ src/routes/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import "./ProfilePage.scss";
+import Chat from "../../components/Chat/Chat";
+import List from "../../components/List/List";
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
-  )
-}
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
 
-export default ProfilePage
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+            </span>
+            <span>
+              Username: <b>John Doe</b>
+            </span>
+            <span>
+              E-mail: <b>john@gmail.com</b>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List/>
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chatComponent">
+        <div className="wrapper">
+          <Chat />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
