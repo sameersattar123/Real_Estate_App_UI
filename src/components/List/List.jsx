7ae0f3b9b07ce0b1162be22b083b70f4f2aa570b
Sameer <sameersattar1111@gmail.com>
2024-03-28 20:50:29 +0500

    list component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {listData} from "../../data/data"
+import Card from "../Card/Card"
+import "./List.scss"
 
 const List = () => {
   return (
-    <div>List</div>
-  )
+    <div className="list">
+      {
+        listData.map((item) => {
+          return (
+            <Card
+            item={item}
+            key={item.id}
+            />
+          )
+        })    
+      }
+    </div>
+  ) 
 }
 
 export default List
